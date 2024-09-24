21defeff3ddbef97365b3029dc5f671c3597830e
Eric Cronje <eric.cronje@gmail.com>
2024-09-24 19:12:17 +0800

    Updated the example Console app.
    The application now returns:
    Items on the page.
    Indicate if it is the first or last page.

--- Examples/PaginationConsole/Program.cs
+++ Examples/PaginationConsole/Program.cs
@@ -4,7 +4,8 @@ const string cCaptionPageSize = "Page size?";
 const string cCaptionTotalCount = "Total items?";
 
 const string cCaptionPage = "Page:";
-//0_
+//0. Add new const for menu option {AD41F151-F4E2-40D0-9572-8C134D10A05B}
+
 const string cInputQuestion = "Choice?";
 const string cOptionExit = "x";
 const string cOptionCaptionExit = " (exit)";
@@ -12,11 +13,18 @@ const string cOptionNext = "n";
 const string cOptionCaptionNext = " (next)";
 const string cOptionBack = "b";
 const string cOptionCaptionBack = " (back)";
+const string cOptionLast = "l";
+const string cOptionCaptionLast = " (last)";
+const string cOptionFirst = "f";
+const string cOptionCaptionFirst = " (first)";
+
 //1. Add new const for cOption<?> and cOptionCaption<?> {4576CA0A-29C1-4469-83C5-E20C29035156}
 
 const string cCaptionInitialInstruction =
     $"{cOptionNext}{cOptionCaptionNext}" +
     $" or {cOptionBack}{cOptionCaptionBack}" +
+    $" or {cOptionFirst}{cOptionCaptionFirst}" +
+    $" or {cOptionLast}{cOptionCaptionLast}" +
     //2. Add new " or {cOption<?>}{cOptionCaption<?>} "OptionCaption {90362A2D-3625-4E8A-8A4E-EA4264A79FE5}
     $" or {cOptionExit}{cOptionCaptionExit}"
     ;
@@ -31,7 +39,7 @@ P pagination = new(pageSize, totalCount);
 
 string? input = string.Empty;
 
-ShowOutput(cCaptionPage, cCaptionInitialInstruction, pagination);
+ShowOutput(pagination);
 
 while (input != cOptionExit)
 {
@@ -46,18 +54,30 @@ while (input != cOptionExit)
         case cOptionBack:
             pagination.GetPreviousPage();
             break;
+        case cOptionLast:
+            pagination.GoToLastPage();
+            break;
+        case cOptionFirst:
+            pagination.GoToFirstPage();
+            break;
         //3. Add new case section; OptionCase {8F2650E5-807C-4B9B-BF2F-1C17CA5BEC29}
         default:
             break;
     }
 
     Console.Clear();
-    ShowOutput(cCaptionPage, cCaptionInitialInstruction, pagination);
+    ShowOutput(pagination);
 }
 
-static void ShowOutput(string cCaptionPage, string cCaptionInitialInstruction, P pagination)
+static void ShowOutput(P pagination)
 {
-    Console.WriteLine($"{cCaptionPage} {pagination.GetPageNumberCaption()}");
+    Console.WriteLine($"{cCaptionPage} " +
+        $"{pagination.GetPageNumberCaption()} " +
+        $"{pagination.GetFirstItemOnPage()}-{pagination.GetLastItemOnThePage()} " +
+        $"({pagination.GetItemCountOnPage()})" + 
+        (pagination.IsFirstPage() ? " First page" : "") +
+        (pagination.IsLastPage() ? " Last page" : "")
+        );
     //4. Add display item {A17784DA-A832-41ED-A613-6D9973213F94}
     Console.WriteLine(cCaptionInitialInstruction);
 }
